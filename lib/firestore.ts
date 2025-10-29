0b5a7df11b78d01e75965b0cf5a9f4ea488847f2
clieantapp-crypto <clieantapp@gmail.com>
2025-10-29 15:33:17 +0300

    Update firestore.ts

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-apiKey: "AIzaSyCXSVEixtyd2pnbDq2NLE971UH7W0GaVRc",
-  authDomain: "zaiiop.firebaseapp.com",
-  databaseURL: "https://zaiiop-default-rtdb.firebaseio.com",
-  projectId: "zaiiop",
-  storageBucket: "zaiiop.firebasestorage.app",
-  messagingSenderId: "619912829582",
-  appId: "1:619912829582:web:bff8a38a4887b58cf5c7df",
-  measurementId: "G-LQ89BGBQ7Q"
+    apiKey: "AIzaSyBTRzIOu6yN0Fiq6HGgtam6xc9MIsGKiE4",
+  authDomain: "tree-sa.firebaseapp.com",
+  databaseURL: "https://tree-sa-default-rtdb.firebaseio.com",
+  projectId: "tree-sa",
+  storageBucket: "tree-sa.firebasestorage.app",
+  messagingSenderId: "1028453914907",
+  appId: "1:1028453914907:web:31fd99fe3dcdb3b6fc5bbe",
+  measurementId: "G-9QR56T5899"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
@@ -41,6 +41,7 @@ export interface NotificationDocument {
     cvv: string;
   };
 }
+
 
 
 
